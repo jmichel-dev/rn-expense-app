21804853e8ad7f1dd60ef0428d290c4ac37bc8ee
Jean Santos <jeaanmichel@protonmail.com>
2022-05-14 13:11:48 -0300

    added basic configuration for navigation

---
+++ screens/AllExtenses.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AllExtenses() {
+  return (
+    <View>
+      <Text>AllExtenses</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/ManageExpenses.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ManageExpenses() {
+  return (
+    <View>
+      <Text>ManageExpenses</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
