36287a2c7d2e90a0185fe0b96cfaff5e2b43750d
Jean Santos <jeaanmichel@protonmail.com>
2022-05-14 13:13:42 -0300

    disable header for stack screens

--- App.js
+++ App.js
@@ -26,7 +26,13 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{
+              headerShown: false,
+            }}
+          />
           <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
         </Stack.Navigator>
       </NavigationContainer>
